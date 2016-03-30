8b7a6fd94b578fb807f8dc54cfeb0f2b7d76bdc1
n0derunner <noderunner@protonmail.com>
2016-03-29 22:04:42 -0500

    Added files via upload

--- client.py
+++ client.py
@@ -3,22 +3,43 @@ import os
 import select
 import sys
 
+<<<<<<< HEAD
+# initial settings
+USER = raw_input("USERNAME: ")
+PORT_S = raw_input("PORT to connect to: ")
+PORT = int(PORT_S)
+HOST = raw_input("IP to connect to: ")
+
+def prompt():
+   sys.stdout.write(USER + "\n")   
+   sys.stdout.flush()
+
+=======
 def prompt():
    sys.stdout.write('<You> ')
    sys.stdout.flush()
 
 HOST = '127.0.0.1'
 PORT = 9050
+>>>>>>> dc299a2ddecd966c18f6ca4e98ece4fa45469a64
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error:
     print 'Failed to create socket'
     sys.exit()
 
+<<<<<<< HEAD
+# attempt connection
+s.connect((HOST, PORT))
+
+# successful connection
+print 'Connected to remote host. Start sending messages'
+=======
 s.connect((HOST, PORT))
 
 print 'Connected to remote host. Start sending messages'
 prompt()
+>>>>>>> dc299a2ddecd966c18f6ca4e98ece4fa45469a64
 
 while 1:
 
@@ -37,5 +58,10 @@ while 1:
                 prompt()
         else:
             msg = sys.stdin.readline()
+<<<<<<< HEAD
+            s.send('\r' + '<' + USER + '>' + msg)
+            prompt()
+=======
             s.send('\r<Client>: ' + msg)
             prompt()
+>>>>>>> dc299a2ddecd966c18f6ca4e98ece4fa45469a64
--- server.py
+++ server.py
@@ -3,27 +3,51 @@ import os
 import select
 import sys
 
+<<<<<<< HEAD
+# Initial settings
+USER = raw_input("What is your user name?: ")
+PORT_S = raw_input("Please enter a port to accept connections: ")
+PORT = int(PORT_S)
+HOST = raw_input("Please enter an IP to connect too?: ")
+RECV_BUFFER = 4096
+
+# Create first prompt
+def prompt():
+    sys.stdout.write(USER)
+    sys.stdout.flush()
+=======
 def prompt():
     sys.stdout.write('<You> ')
     sys.stdout.flush()
 
+>>>>>>> dc299a2ddecd966c18f6ca4e98ece4fa45469a64
 try:
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except:
     print 'Failed to create socket'
     sys.exit()
 
+<<<<<<< HEAD
+# Creating socket connection server-side
+=======
 PORT = 9050
 HOST = '127.0.0.1'
 RECV_BUFFER = 4096
 
+>>>>>>> dc299a2ddecd966c18f6ca4e98ece4fa45469a64
 server_socket.bind((HOST, PORT))
 server_socket.listen(10)
 
 input = [server_socket, sys.stdin]
+<<<<<<< HEAD
+prompt()
+
+print 'Chat Program'
+=======
 
 print 'Chat Program'
 prompt()
+>>>>>>> dc299a2ddecd966c18f6ca4e98ece4fa45469a64
 
 while 1:
 
@@ -34,15 +58,30 @@ while 1:
         if sock == server_socket:
             client, address = server_socket.accept()
             input.append(client)
+<<<<<<< HEAD
+=======
             #data = sock.recv(RECV_BUFFER)
             #if data:
                 #sys.stdout.write(data)
         else:
             data = sock.recv(RECV_BUFFER)
+>>>>>>> dc299a2ddecd966c18f6ca4e98ece4fa45469a64
         elif sock == sys.stdin:
             data = sock.readline()
             for s in input:
                 if s not in (server_socket, sys.stdin):
+<<<<<<< HEAD
+                    s.send(data)    
+
+        else:
+            data = sock.recv(RECV_BUFFER)
+            if data:
+                sys.stdout.write(data)
+            else:
+                msg = sys.stdin.readline()
+                server_socket.send('\r' + '<' + USER + '>' + msg)
+                prompt()
+=======
                     s.send(data)
             else:
                 msg = sys.stdin.readline()
@@ -52,3 +91,4 @@ while 1:
 
 
 server_socket.close()
+>>>>>>> dc299a2ddecd966c18f6ca4e98ece4fa45469a64
